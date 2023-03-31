3e2c2d199dd6f846ff4f3e41de7189771abbef70
Asad javed <ajaved01@teresol.com>
2023-03-31 09:29:30 +0500

    1.0.0

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,155 @@
+<template>
+  <div id="app">
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search......." v-model="query"
+        @keypress="fetchWeather">
+      
+      </div>
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Pakistan,Islamabad</div>
+          <div class="date">Thursday March 30,2023</div>
+        </div>
+        <div class="weather-box">
+          <div class="temp">21°C</div>
+          <div class="weather">Rain</div>
+        </div>
+      </div>
+    </main>
+  </div>
+  >
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      api_key: "5b0600b6c0aebd04b20ece8befab2fbc",
+      url_base:'https://api.openweathermap.org/data/2.5/',
+      //data which we search in search bar
+      query:'',
+      //get data from API and store in weather variable
+      weather:{}
+    };
+  },
+  methods:{
+    fetchWeather(e){
+      if(e.key=="Enter"){
+        fetch(`${this.url_base}weather?q=${this.query}&units=matric&APPID=${this.api_key}`)
+
+        //callback function
+        .then(res=>{
+          return res.json()
+        }).then(this.setResults)
+      }
+},
+setResults(results){
+  this.weather=results;
+}
+  }
+
+
+};
+</script>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: "moutserrat", sans-serif;
+}
+#app {
+  /* background-image: url('./assets/cold-bg.png'); */
+  background-image: url("../assets/cold-bg.png");
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+main {
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0, 0.75)
+  );
+}
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+  display: block;
+  padding: 15px;
+  width: 100%;
+
+  color: #313131;
+  font-size: 20px;
+
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+/*
+After clicking on search bar background color or shadow of search bar is changed
+*/
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+.location-box .location{
+  color: #FFF;
+  font-size: 20px;
+  font-weight: 300;
+  text-align: center;
+  font-style: italic;
+  text-shadow: 1px 3px rgba(0,0,0,0.25);
+}
+.location-box .date{
+  color: #FFF;
+  font-size: 20px;
+  font-weight: 300;
+  text-align: center;
+  font-style: italic;
+}
+.weather-box{
+  text-align: center;
+}
+.weather-box .temp{
+  display: inline-block;
+  padding: 10px 25px;
+  color: #FFF;
+  font-size: 102px;
+  font-weight: 900;
+
+
+
+
+  text-shadow:  3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius:16px;
+  margin: 30px 0px;
+
+  box-shadow: 3px 6px rgba(0,0,0,0.25);
+}
+.weather-box .weather{
+  color: #FFF;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0,0,0,0.25);
+}
+</style>
+
